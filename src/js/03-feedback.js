ebdfd60057adf6073c62fe8ed7d46f90f49cd0a1
Andrii Zorba <andrej.zorba@gmail.com>
2022-10-14 23:55:39 +0300

    add correction to task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,28 +2,29 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+console.log(`~ savedData`, savedData);
 
 fillTextareaForm();
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onFormText, 500));
 
 function fillTextareaForm() {
-  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  if (savedData) {
-    form.elements.email.value = savedData.email;
-    form.elements.message.value = savedData.message;
+  for (let key of Object.keys(savedData)) {
+    if (savedData[key]) {
+      form.elements[key].value = savedData[key];
+    }
   }
 }
 
 function onFormSubmit(event) {
   event.preventDefault();
-  console.table(formData);
+  console.table(savedData);
   event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
 
 function onFormText(event) {
-  formData[event.target.name] = event.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  savedData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(savedData));
 }

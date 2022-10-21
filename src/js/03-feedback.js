ab44abbadb37d6540af31a61c7f3c78f291b8316
Andrii Zorba <andrej.zorba@gmail.com>
2022-10-21 19:34:54 +0300

    add correction

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,14 +2,13 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
-const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
-console.log(`~ savedData`, savedData);
 
 fillTextareaForm();
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onFormText, 500));
 
 function fillTextareaForm() {
+  const savedData = getLocalData();
   for (let key of Object.keys(savedData)) {
     if (savedData[key]) {
       form.elements[key].value = savedData[key];
@@ -19,12 +18,18 @@ function fillTextareaForm() {
 
 function onFormSubmit(event) {
   event.preventDefault();
+  const savedData = getLocalData();
   console.table(savedData);
   event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
 
 function onFormText(event) {
+  const savedData = getLocalData();
   savedData[event.target.name] = event.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(savedData));
+}
+
+function getLocalData() {
+  return JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 }
